5dd315e0b5e39a058c96368470710be59736af04
geebabee <manchagrace@gmail.com>
2023-04-28 19:37:19 -0700

    printf

--- _printflast.c
+++ _printflast.c
@@ -1,42 +1,28 @@
 #include "main.h"
 
 /**
-
- * handle_print - Prints an argument based on its type
-
- * @fmt: Formatted string in which to print the arguments.
-
- * @list: List of arguments to be printed.
-
+ * printf_last - Print argument based on type
+ * @fmt: Formatted string to print the arguments.
+ * @list: List of arguments  printed.
  * @ind: ind.
-
- * @buffer: Buffer array to handle print.
-
- * @flags: Calculates active flags
-
+ * @buffer: Buffer array
+ * @flags: Calculate active flags
  * @width: get width.
-
  * @precision: Precision specification
-
  * @size: Size specifier
-
  * Return: 1 or 2;
-
- */
-
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-
-	int flags, int width, int precision, int size)
-
+*/
+int printf_last(const char *fmt, int *ind, va_list list, char buffer[],
+		int flags, int width, int precision, int size)
 {
 
-	int i, unknow_len = 0, printed_chars = -1;
+	int a, unknow_len = 0, printed_chars = -1;
 
-	fmt_t; fmt_types[] = {
+	fmt_t fmt_types[] = {
 
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 
-		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'a', print_int}, {'d', print_int}, {'b', print_binary},
 
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 
@@ -46,51 +32,27 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 
 	};
 
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-
-		if (fmt[*ind] == fmt_types[i].fmt)
-
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-
-
-	if (fmt_types[i].fmt == '\0')
-
+	for (a = 0; fmt_types[a].fmt != '\0'; a++)
+		if (fmt[*ind] == fmt_types[a].fmt)
+			return (fmt_types[a].fn(list, buffer, flags, width, precision, size));
+	if (fmt_types[a].fmt == '\0')
 	{
-
 		if (fmt[*ind] == '\0')
-
 			return (-1);
-
 		unknow_len += write(1, "%%", 1);
-
 		if (fmt[*ind - 1] == ' ')
-
 			unknow_len += write(1, " ", 1);
-
 		else if (width)
-
 		{
-
 			--(*ind);
-
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-
 				--(*ind);
-
 			if (fmt[*ind] == ' ')
-
 				--(*ind);
-
 			return (1);
-
 		}
-
 		unknow_len += write(1, &fmt[*ind], 1);
-
 		return (unknow_len);
-
 	}
-
 	return (printed_chars);
-
 }
